00e21cd8ab2b456bdab5fb2ce8fdb0a869dc1ee0
Ural-viking <i-didder@yandex.ru>
2023-04-16 00:18:43 +0500

    first commit

---
+++ src/Task_1000.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task_1000 {
+    public static void main(String[] args){
+        Scanner scr = new Scanner(System.in);
+        long a, b;
+        a = scr.nextLong();
+        b = scr.nextLong();
+        System.out.print(a+b);
+    }
+}
